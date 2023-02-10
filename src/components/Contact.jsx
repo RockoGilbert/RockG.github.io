206a6abd867dbc86543512b3df088e658b154416
RockoGilbert <evolve2122@gmail.com>
2023-02-09 20:57:30 -0600

    Added path to site

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
 
+
+const styles = {
+  container: {
+    marginTop: "95px",
+    "@media (max-width: 940px)": {
+      marginTop: "50px",
+      paddingTop: "50px"
+    }
+  }
+};
+
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-90vh bg-[#07224f] items-start p-15'>
-        <div className="flex justify-center items-center p-4 pt-6">
-          <form method='POST' action="https://getform.io/f/f809a263-7fab-4170-8e8d-a8959d823856" className='flex flex-col max-w-[600px] w-full text-black'>
-              <div className='pb-8'>
-                  <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-                  <p className='text-gray-600 py-4'> Submit the form below or shoot me an email - evolve2122@gmail.com</p>
-              </div>
-              <input className='text-black bg-[#ccd6f6] p-2' type="text" placeholder='Enter your Name' />
-              <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Enter your Email' />
-              <textarea className='bg-[#ccd6f6] p-2  text-gray-900' name="message" rows="10" placeholder='Message'></textarea>
-              <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
-          </form>          
-        </div>
+    <div style={styles.container} name='contact' className='w-full h-90vh bg-[#07224f] items-start p-15'>
+      <div className="flex justify-center items-center p-4 pt-6">
+        <form method='POST' action="https://getform.io/f/f809a263-7fab-4170-8e8d-a8959d823856" className='flex flex-col max-w-[600px] w-full text-black'>
+          <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
+            <p className='text-gray-600 py-4'> Submit the form below or shoot me an email - evolve2122@gmail.com</p>
+          </div>
+          <input className='text-black bg-[#ccd6f6] p-2' type="text" placeholder='Enter your Name' />
+          <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Enter your Email' />
+          <textarea className='bg-[#ccd6f6] p-2  text-gray-900' name="message" rows="10" placeholder='Message'></textarea>
+          <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+        </form>
+      </div>
     </div>
   )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import smiling_cover from "../components/smiling-cover.png";
-import AirBnB from "../components/AirBnb.png";
+import travel_kenz from "../components/travel-kenz.png";
 import Kahuna from "../components/kahuna.png";
 
 const Projects = () => {
   return (
     <div name="Projects" className="w-80% h-screen bg-[#0a192f] text-gray-200">
-      <div className="flex flex-col sm:flex justify-center items-center w-70% h-full">
+      <div className="flex flex-col sm:flex justify-center items-center w-70% 200px">
         <div className="max-w-3xl grid grid-cols-1 gap-8">
           <div className="md:mb-6 mt-6 pt-20 sm:text-left pt-35 pb-8 pl-4">
             <p className="text-4xl font-bold inline border-b-4 border-pink-600">
@@ -14,14 +14,14 @@ const Projects = () => {
             </p>
           </div>
         </div>
-        <div className="w-3Learni grid grid-cols-3 gap-10 text-center ml-10 py-10">
+        <div className="w-3Learni grid grid-cols-3 gap-10 text-center pl-10 py-10">
           <div className="w-3/4 shadow-md shadow-[#040c16] hover:Scale-110 duration-500">
             <a
               href="https://github.com/RockoGilbert/holberton-smilingschool-javascript"
               class="mx-5 animate-pulse text-red-400 mx-3">Smiling School
             </a>
-            <img className="object-cover w-3/4 h-60 mx-auto mt-7" src={smiling_cover} alt="smiling-cover" />
-            <p className="my-8 pl-14 ">
+            <img className="object-cover w-3/4 h-41 mx-auto mt-7" src={smiling_cover} alt="smiling-cover" />
+            <p className="my-8 pl-14 pr-10">
               Dynamic web application that was built using JavaScript, jQuery,
               Bootstrap, and UI/UX designs.
             </p>
@@ -32,7 +32,7 @@ const Projects = () => {
               href="https://github.com/RockoGilbert/kahuna"
               class="animate-pulse text-red-400">Kahuna
             </a>
-            <img className="object-cover w-3/4 h-60 mx-auto mt-7" src={Kahuna} alt="Kahuna" />
+            <img className="object-cover w-3/4 h-41 mx-auto mt-7" src={Kahuna} alt="Kahuna" />
             <p className="my-8">
               Custom front-end from a prompt “waves”. Created using HTML, CSS,
               Vanilla JS. and Tailwind. Fully responsive with search features.
@@ -41,13 +41,12 @@ const Projects = () => {
 
           <div className="shadow-md shadow-[#040c16] hover:Scale-110 duration-500">
             <a
-              href="https://github.com/RockoGilbert/AirBnB_clone_v4"
-              class="animate-pulse text-red-400">AirBnb
+              href="https://travel-kenz.netlify.app"
+              class="animate-pulse text-red-400">Travel_Kenz
             </a>
-            <img className="object-cover w-full h-60 mx-auto mt-7" src={AirBnB} alt="AirBnb" />
+            <img className="object-cover w-3/4 h-60 mx-auto mt-7" src={travel_kenz} alt="Travel Kenz" />
             <p className="my-8">
-              Airbnb-like web-service built with Python, MySQL, Flask, NGINX,
-              and JavaScript
+              A React Js front-end
             </p>
           </div>
         </div>
